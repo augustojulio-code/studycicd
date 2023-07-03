b18ff68a9418d284d2c4b1af0510a9c97232806a
Julio <julio.mech@hotmail.com>
2023-07-03 09:46:27 -0300

    v0.1 First Commit

---
+++ projectseleniumtest/src/test/java/br/com/julioproject/Automation.java
@@ -0,0 +1,26 @@
+package br.com.julioproject;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Automation {
+
+    @Test
+    public void SearchGoogle() {
+        System.setProperty("webdriver.chrome.driver", "src\\drive\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://google.com");
+
+        driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/textarea")).click();
+        driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/textarea"))
+                .sendKeys("Redit", Keys.ENTER);
+        driver.findElement(By.xpath(
+                "/html/body/div[6]/div/div[13]/div[1]/div[2]/div[2]/div/div/div[1]/div/div/div/div/div/div/div/div[1]/a"))
+                .click();
+    }
+}
