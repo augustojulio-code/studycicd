ecc83debe2bfa5bf25a3278c5c3ea8ee28a6a38c
Julio <julio.mech@hotmail.com>
2023-07-06 13:14:51 -0300

    v0.1.1 testing cucumber

--- projectseleniumtest/src/test/java/br/com/julioproject/AppTest.java
+++ projectseleniumtest/src/test/java/br/com/julioproject/AppTest.java
@@ -7,14 +7,22 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testAbc() {
+        String test = "ok";
+        if (test.equalsIgnoreCase("ok")) {
+            assertTrue(true);
+        } else {
+            assertTrue(false);
+        }
     }
 }
